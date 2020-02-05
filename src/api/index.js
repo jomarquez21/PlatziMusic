6e994c637527868cdbc05e70d38f462a1a1fd5a8
Jose Enrique Marquez <jmrqz21@gmail.com>
2020-02-05 08:09:06 -0500

    correction protocol url

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import API from './config';
 const {apiKey} = API;
 
 export function getArtists(country) {
-  const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${apiKey}&format=json`;
+  const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${apiKey}&format=json`;
 
   return fetch(URL)
     .then(response => response.json())
